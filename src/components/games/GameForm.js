f07b36bb2bc2f3dfc3dbc419e47e5abf5e267282
Nick Carver <nicholascarver2@outlook.com>
2021-05-14 12:06:16 -0500

    got the form to work correctly for creating a new game

--- src/components/games/GameForm.js
+++ src/components/games/GameForm.js
@@ -3,12 +3,17 @@ import { GameContext } from "./GameProvider.js"
 import { useHistory, useParams } from 'react-router-dom'
 import { CategoryContext } from "../categories/CategoryProvider.js"
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import { Multiselect } from 'multiselect-react-dropdown';
 
 export const GameForm = () => {
     const history = useHistory()
     const { createGame, getGameById, modifyGame } = useContext(GameContext)
     const { getCategories, categories } = useContext(CategoryContext)
     const { gameId } = useParams()
+
+    const state = {
+        options: categories
+    };
 
     const [currentGame, setCurrentGame] = useState({
         title: "",
@@ -39,7 +44,23 @@ export const GameForm = () => {
 
     const handleInputChange = (event) => {
         const newGameState = { ...currentGame }
-        newGameState.title = event.target.value
+        newGameState[event.target.name] = event.target.value
+        setCurrentGame(newGameState)
+    }
+
+    const onSelect = (selectedList, selectedItem) => {
+        const newGameState = { ...currentGame }
+        newGameState.categories.push(selectedItem.id)
+        setCurrentGame(newGameState)
+        console.log(newGameState)
+    }
+
+    const onRemove = (selectedList, removedItem) => {
+        const newGameState = { ...currentGame }
+        const index = newGameState.categories.indexOf(removedItem.id);
+        if (index > -1) {
+            newGameState.categories.splice(index, 1);
+        }
         setCurrentGame(newGameState)
     }
 
@@ -59,7 +80,7 @@ export const GameForm = () => {
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Designer: </label>
-                    <input type="text" name="title" required className="form-control"
+                    <input type="text" name="designer" required className="form-control"
                         value={currentGame.designer}
                         onChange={handleInputChange}
                     />
@@ -68,16 +89,26 @@ export const GameForm = () => {
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Release Year: </label>
-                    <input type="text" name="title" required className="form-control"
+                    <input type="text" name="yearReleased" required className="form-control"
                         value={currentGame.yearReleased}
                         onChange={handleInputChange}
                     />
                 </div>
             </fieldset>
+            <FormGroup>
+                <Label for="exampleSelectMulti">Select Categories:</Label>
+                <Multiselect
+                    options={state.options} // Options to display in the dropdown
+                    selectedValues={state.selectedValue} // Preselected value to persist in dropdown
+                    onSelect={onSelect} // Function will trigger on select event
+                    onRemove={onRemove} // Function will trigger on remove event
+                    displayValue="category" // Property name to display in the dropdown options
+                />
+            </FormGroup>
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Number of Players: </label>
-                    <input type="number" name="title" required className="form-control"
+                    <input type="number" name="numberOfPlayers" required className="form-control"
                         value={currentGame.numberOfPlayers}
                         onChange={handleInputChange}
                     />
@@ -86,38 +117,20 @@ export const GameForm = () => {
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Time To Play: </label>
-                    <input type="number" required className="form-control" value={currentGame.timeToPlay} onChange={handleInputChange} />
+                    <input name="timeToPlay" type="number" required className="form-control" value={currentGame.timeToPlay} onChange={handleInputChange} />
                 </div>
             </fieldset>
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="title">Minimum Age: </label>
-                    <input type="number" required className="form-control" value={currentGame.minAgeRecommendation} onChange={handleInputChange} />
+                    <input name="minAgeRecommendation" type="number" required className="form-control" value={currentGame.minAgeRecommendation} onChange={handleInputChange} />
                 </div>
             </fieldset>
-            <FormGroup>
-                <Label for="exampleSelectMulti">Select Categories:</Label>
-                <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                    <option value={0}>Select a Category...</option>
-                    {categories.map(category => {
-                        return <option key={category.id} value={category.id}>{category.category}</option>
-                    })}
-                </Input>
-            </FormGroup>
             {gameId ? <button type="submit"
                 onClick={evt => {
                     // Prevent form from being submitted
                     evt.preventDefault()
-                    const game = {
-                        id: currentGame.id,
-                        maker: currentGame.maker,
-                        title: currentGame.title,
-                        numberOfPlayers: parseInt(currentGame.numberOfPlayers),
-                        skillLevel: parseInt(currentGame.skillLevel),
-                        gameTypeId: parseInt(currentGame.gameTypeId)
-                    }
-                    // Send POST request to your API
-                    modifyGame(game)
+                    modifyGame(currentGame)
                         .then(() => history.push("/games"))
                 }}
                 className="btn btn-primary">Submit Edit</button>
@@ -126,15 +139,8 @@ export const GameForm = () => {
                     onClick={evt => {
                         // Prevent form from being submitted
                         evt.preventDefault()
-                        const game = {
-                            maker: currentGame.maker,
-                            title: currentGame.title,
-                            numberOfPlayers: parseInt(currentGame.numberOfPlayers),
-                            skillLevel: parseInt(currentGame.skillLevel),
-                            gameTypeId: parseInt(currentGame.gameTypeId)
-                        }
                         // Send POST request to your API
-                        createGame(game)
+                        createGame(currentGame)
                             .then(() => history.push("/games"))
                     }}
                     className="btn btn-primary">Create</button>}
